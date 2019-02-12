1264cf73620434907e4e72bd179639c3280f5aa6
Mark <developerzero7@gmail.com>
2019-02-12 14:03:26 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const winston = require("winston");
+const express = require("express");
+const config = require("config");
+const path = require("path");
+const app = express();
+
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
+
+if (process.env.NODE_ENV === "production") {
+  // serve static files from the React app
+  app.use(express.static("client/build"));
+
+  // The "catchall" handler: for any request that doesn't
+  // match one above, send back React's index.html file.
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
+
+module.exports = server;
